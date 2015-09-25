6147e1f18c4d2d098390ee0698ba6e1ff54b7a67
buddybuild <help@buddybuild.com>
2015-09-25 00:08:34 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- FlappyBird/AppDelegate.swift
+++ FlappyBird/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         return true
     }
 
