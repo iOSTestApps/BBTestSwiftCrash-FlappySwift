48dbd35a3affa2fbd5e8c8c7eba4a626f84973a6
Rashin Arab <rashin@buddybuild.com>
2015-09-24 23:58:46 -0700

    Removing buddybuild sdk

--- FlappyBird/AppDelegate.swift
+++ FlappyBird/AppDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        BuddyBuildSDK.setup()
         return true
     }
 
